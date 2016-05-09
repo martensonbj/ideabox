f2dcc736da8b59086c4038c5b46e5d7c538fea84
Brenna Martenson <martensonbj@gmail.com>
2016-05-09 15:58:06 -0600

    Debugging sorting by quality

--- app/assets/javascripts/sort_ideas.js
+++ app/assets/javascripts/sort_ideas.js
@@ -1,28 +1,26 @@
 $(document).ajaxSuccess(function(){
-  var qualities = {"genius": 2, "plausible" : 1, "swill" : 0}
+  // var $qualities = {"genius": 2, "plausible" : 1, "swill" : 0}
   var $ideas = $('.item')
   console.log('ideas at first:' + $ideas)
 
-  function sortIdeas(){
-    $('#sort-ideas').on('click', function(){
-      console.log("in sort ideas function")
-      orderByQuality();
-    });
-  }
+  $('#sort-ideas').on('click', function(){
+    console.log("in sort ideas function")
+    orderByQuality();
+  });
 
   function orderByQuality() {
+    console.log('orderbyQuality');
     var orderedIdeas = $ideas.sort(function (a, b) {
-      var qualityName = $idea.find('.quality').text().toLowerCase();
-      qualities.qualityName
+      var qualityName = $ideas.find('.quality').text().toLowerCase();
       return a > b
-      console.log('ideas after: ' + $ideas)
+      console.log('ideas after: ' + $ideas);
     });
     resetIdeas(orderedIdeas);
   }
 
   function resetIdeas(orderedIdeas) {
     $('.idea').remove();
-    orderedIdeas.each(ideas, function(index, idea){
+    orderedIdeas.each( function(index, idea){
       $('#ideas-list').append(idea);
     });
   }
