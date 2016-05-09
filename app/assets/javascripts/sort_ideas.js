618468226a27e8f1050ceccc7b7e71d88faa9f36
Brenna Martenson <martensonbj@gmail.com>
2016-05-09 15:27:55 -0600

    Refactor sort functionality

--- app/assets/javascripts/sort_ideas.js
+++ app/assets/javascripts/sort_ideas.js
@@ -1,15 +1,30 @@
 $(document).ajaxSuccess(function(){
   var qualities = {"genius": 2, "plausible" : 1, "swill" : 0}
   var $ideas = $('.item')
+  console.log('ideas at first:' + $ideas)
 
   function sortIdeas(){
     $('#sort-ideas').on('click', function(){
-      $ideas.sort(function (a, b) {
-        var qualityName = $idea.find('.quality').text().toLowerCase();
-        qualities.qualityName
-        return a > b
-      });
-      console.log($ideas)
+      console.log("in sort ideas function")
+      orderByQuality();
     });
   }
-})
+
+  function orderByQuality() {
+    var orderedIdeas = $ideas.sort(function (a, b) {
+      var qualityName = $idea.find('.quality').text().toLowerCase();
+      qualities.qualityName
+      return a > b
+      console.log('ideas after: ' + $ideas)
+    });
+    resetIdeas(orderedIdeas);
+  }
+
+  function resetIdeas(orderedIdeas) {
+    $('.idea').remove();
+    orderedIdeas.each(ideas, function(index, idea){
+      $('#ideas-list').append(idea);
+    });
+  }
+
+});
